639aab04863bc14f2e8d0e2d018947b925409181
Billy272 <billylaroi@gmail.com>
2024-06-10 20:08:57 +0300

    commit

--- taxi1_app/lib/home_screen.dart
+++ taxi1_app/lib/home_screen.dart
@@ -11,160 +11,18 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   static const _initialCameraPosition = CameraPosition(
-    target: LatLng(1.2921, 36.8219),
+    target: LatLng(-1.2921, 36.8219),
     zoom: 11.5,
   );
 
-  late GoogleMapController _googleMapController;
-  late Marker _origin;
-  late Marker _destination;
-  late Directions _info;
-
-  @override
-  void dispose() {
-    _googleMapController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Taxi App'),
-          actions: [
-            if (_origin != null)
-              TextButton(
-                onPressed: ()  => _googleMapController.animateCamera(
-                CameraUpdate.newCameraPosition(
-                  CameraPosition(
-                  target: _origin.position,
-                  zoom: 14.5,
-                  tilt: 50.0,
-                  ),
-                ),
-              ),
-            style: TextButton.styleFrom(
-            foregroundColor: Colors.blue,
-                backgroundColor: const Color.fromARGB(255, 0, 0, 55),
-          ),
-          if (_destination != null)
-            TextButton(
-              onPressed: ()  => _googleMapController.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                  target: _destination.position,
-                  zoom: 14.5,
-                  tilt: 50.0,
-                ),
-              ),
-            ),
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.greenAccent, backgroundColor: const Color.fromARGB(255, 0, 0, 55)
-            ),
-            ]
-            ),
-      body: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[GoogleMap(
+    return const Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
         myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        initialCameraPosition: _initialCameraPosition ,
-        onMapCreated: (controller) => _googleMapController = controller,
-        markers: {
-          if (_origin != null) _origin,
-          if (_destination != null) _destination,
-        },
-        polylines: {
-          if (_info != null){
-            Polyline(
-              polylineId: const PolylineId('overview_polyline'),
-              color: Colors.blueAccent,
-              width: 5,
-              points: _info.polylineCoordinates
-                  .map((e) => LatLng(e.latitude, e.longitude))
-                  .toList(),
-            ),
-          },
-    }
-    }
-        onLongPress: _addMarker,
       ),
-      if (_info == null) {
-        Positioned(
-          top: 20.0,
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 6.0,
-              horizontal: 12.0,
-            ),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 0, 0, 55),
-              borderRadius: BorderRadius.circular(20.0),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0, 2),
-                  blurRadius: 6.0,
-                ),
-              ],
-            ),
-            child: Text(
-              '${_info.totalDistance}, ${_info.totalDuration}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-                fontWeight: FontWeight.w600,
-              ),
-    ),
-          ),
-        )
-    }
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 0, 0, 55),
-        foregroundColor: Colors.white,
-        onPressed: () => _googleMapController.animateCamera(
-          _info != null
-              ? CameraUpdate.newLatLngBounds(_info.bounds, 100.0)
-              : CameraUpdate.newCameraPosition(_initialCameraPosition),
-        ),
-        child: const Icon(Icons.center_focus_strong),
-      ),
-    ],
-    ),
     );
   }
-  void _addMarker (LatLng pos) async {
-    if (_origin == null || (_origin != null && _destination != null)) {
-      setState(() {
-        _origin = Marker(
-          markerId: const MarkerId('origin'),
-          infoWindow: const InfoWindow(title: 'Origin'),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-          position: pos,
-        );
-        _destination = null;
-
-        _info = null;
-      });
-    } else {
-      setState(() {
-        _destination = Marker(
-          markerId: const MarkerId('destination'),
-          infoWindow: const InfoWindow(title: 'Destination'),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-          position: pos,
-        );
-      });
-
-      final directions = await Directions() .getDirections(
-        origin: _origin.position,
-        destination: _destination.position,
-      );
-      setState(() => _info = directions);
-    }
-  }
 }
-
-
-
--- taxi1_app/lib/main.dart
+++ taxi1_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
 import 'package:taxi1_app/login.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:taxi1_app/home_screen.dart';
@@ -6,6 +7,22 @@ import 'package:taxi1_app/wallet.dart';
 import 'package:taxi1_app/profile.dart';
 
 void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Material(
+      child: Container(
+        color: Colors.blueGrey,
+        child: const Text(
+          'An error occurred. Please restart the app.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontStyle: FontStyle.italic,
+          ),
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  };
   runApp(const MyApp());
 }
 
